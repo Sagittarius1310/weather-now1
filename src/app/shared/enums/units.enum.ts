0af64185033555e5023a73c338e996f69c729bd0
Oleksii Hilenko <aleksey.gilenko@gmail.com>
2026-05-07 00:16:49 +0300

    feat(units): add units enums

---
+++ src/app/shared/enums/units.enum.ts
@@ -0,0 +1,14 @@
+export enum TemperatureUnit {
+    Celsius = 'celsius',
+    Fahrenheit = 'fahrenheit'
+}
+
+export enum WindSpeedUnit {
+    KmH = 'km/h',
+    Mph = 'mph'
+}
+
+export enum PrecipitationUnit {
+    Millimeters = 'millimeters',
+    Inches = 'inches'
+}
