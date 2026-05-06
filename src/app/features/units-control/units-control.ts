afbb142491fdff16298711a7729e724e89fcc41c
Oleksii Hilenko <aleksey.gilenko@gmail.com>
2026-05-07 00:19:43 +0300

    feat(units): add units dropdown trigger button and unit options to select

--- src/app/features/units-control/units-control.ts
+++ src/app/features/units-control/units-control.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { TemperatureUnit, WindSpeedUnit, PrecipitationUnit } from '@shared/enums/units.enum';
 
 @Component({
     selector: 'app-units-control',
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './units-control.html',
     styleUrl: './units-control.scss',
+    host: { class: 'units-control' },
 })
-export class UnitsControl {}
+export class UnitsControl {
+    // Expose the enums to the template
+    protected readonly TemperatureUnit = TemperatureUnit;
+    protected readonly WindSpeedUnit = WindSpeedUnit;
+    protected readonly PrecipitationUnit = PrecipitationUnit;
+
+    // Dropdown state
+    protected isOpen: boolean = false;
+
+    // Selected units
+    protected selectedTemp = TemperatureUnit.Celsius;
+    protected selectedWindSpeed = WindSpeedUnit.KmH;
+    protected selectedPrecipitation = PrecipitationUnit.Millimeters;
+
+    // Close dropdown on click outside of it
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent): void {
+        if (!(event.target as HTMLElement).closest('app-units-control')) {
+            this.isOpen = false;
+        }
+    }
+
+    protected toggleDropdown(): void {
+        this.isOpen = !this.isOpen;
+    }
+
+    protected toggleUnits(): void {
+        if (this.isMetric) {
+            this.selectedTemp = TemperatureUnit.Fahrenheit;
+            this.selectedWindSpeed = WindSpeedUnit.Mph;
+            this.selectedPrecipitation = PrecipitationUnit.Inches;
+        } else {
+            this.selectedTemp = TemperatureUnit.Celsius;
+            this.selectedWindSpeed = WindSpeedUnit.KmH;
+            this.selectedPrecipitation = PrecipitationUnit.Millimeters;
+        }
+    }
+
+    protected get toggleButtonText(): string {
+        return this.isMetric ? 'Switch to Imperial' : 'Switch to Metric';
+    }
+
+    private get isMetric(): boolean {
+        return (
+            this.selectedTemp === TemperatureUnit.Celsius &&
+            this.selectedWindSpeed === WindSpeedUnit.KmH &&
+            this.selectedPrecipitation === PrecipitationUnit.Millimeters
+        );
+    }
+}
