d613b440ca013ea79789cf6955ea87640d0e92de
Oleksii Hilenko <aleksey.gilenko@gmail.com>
2026-05-09 02:19:28 +0300

    refactor(header): replace a selector with an attribute to ensure HTML semantics

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { UnitsControl } from '@features/units-control/units-control';
 
 @Component({
-    selector: 'app-header',
+    selector: 'header[app-header]',
     imports: [UnitsControl],
     templateUrl: './header.html',
     styleUrl: './header.scss',
